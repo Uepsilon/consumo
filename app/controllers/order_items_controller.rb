294da75bc61aaf20fb48fa10b94fb1054bdee83c
Christoph Bajohr <christoph@bajohr.org>
2014-04-11 18:26:04 +0200

    adds last delivery to quick dashboard

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,6 +3,7 @@ class OrderItemsController < ApplicationController
   def new
     @order_item = OrderItem.new
     @user = User.find(current_user.id)
+    @last_delivery = Delivery.order('created_at ASC').last.product.name
     @bookings = @user.bookings.all
     @bookings_today = @user.bookings.where("created_at >= ?", Time.zone.now.beginning_of_day)
   end

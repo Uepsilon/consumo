9f6e8280c79786d01c9508097b6a9cd961f19054
Uepsilon <maxim.kuepper@gmail.com>
2014-07-10 17:24:37 +0200

    adds alternate source for secret keys

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Consumo::Application.config.secret_token = Rails.application.secrets.secret_token
+Consumo::Application.config.secret_token = Rails.application.secrets.secret_token || ENV['SECRET_TOKEN']
